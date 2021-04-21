5188e94716145267a57faf2a7dbe0203bcc36f0c
Regis Oliveira <regis.aoc@gmail.com>
2021-04-21 19:28:09 -0300

    Aplicando typescript no index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
+import { GetStaticProps } from 'next';
+import { type } from 'node:os';
 
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  // ...
+}
 
-export default function Home(props) {
+type HomeProps = {
+  // episodes: Array<Episode>;
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
 
   return (
     <div>
@@ -12,8 +25,8 @@ export default function Home(props) {
 
 // SSG - Server Side Generation - Este recurso só funciona em produção (build)
 // nesta chamada a página é pré montada, já fica pronta esperando a chamada, muito utilizado em api's que não sofre alteração constante em seus dados.
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes') // busca todos os dados disponibilizado por esta consulta
   const data = await response.json()
 
   return {
